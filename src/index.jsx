c26e4d20adb66dc627482415bf4e77a8da4c1ab9
sorad <soradimichi@gmail.com>
2021-03-21 10:44:00 +0300

    Вернул браузер роутер, пределал конфиг nginx

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
